79c38987797111c9857213460e55f11dcbda7d46
J. Tran <j.yn.tran@gmail.com>
2016-10-24 14:57:33 -0400

    Added splash, location detection, more show info

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,37 @@
-/**
- * Welcome to Pebble.js!
- *
- * This is where you write your app.
- */
-
 var UI = require('ui');
 var ajax = require('ajax');
 
+var geoOptions = {
+  enableHighAccuracy: true,
+  maximumAge: 10000,
+  timeout: 10000
+};
+
+var splash = new UI.Card({
+  title: 'TVmaze Today',
+  subtitle: 'Fetching location...'
+});
+
+splash.show();
+
+navigator.geolocation.getCurrentPosition(success,                                         
+function (err) {
+  console.log('location error (' + err.code + '): ' + err.message);
+  var error = new UI.Card({
+    title: 'Error',
+    body: 'Could not determine location.'
+  });
+  error.show();
+}, geoOptions);
+
+function success(pos) {
+  var lat = pos.coords.latitude;
+  var lon = pos.coords.longitude;
+  var country_code, country;
+
+var tvmaze = function() {  
 ajax({
-  url: 'http://api.tvmaze.com/schedule?country=CA',
+  url: 'http://api.tvmaze.com/schedule?country=' + country_code,
   type: 'json'
 },
 function(data) {
@@ -16,13 +39,15 @@ function(data) {
     return {
       show: obj.show.name,
       name: obj.name,
-      airtime: obj.airtime,
-      runtime: obj.runtime
+      network: obj.show.network.name,
+      airtime: obj.airtime ? obj.airtime : 'N/A',
+      runtime: obj.runtime ? obj.runtime : 'N/A',
     };
   });
   
   var menu = new UI.Menu({
     sections: [{
+      title: country,
       items: schedule.map(function(obj) {
         return {
           title: obj.show,
@@ -37,13 +62,35 @@ function(data) {
     var card = new UI.Card({
       title: item.show,
       subtitle: item.name,
-      body: 'Time: ' + item.airtime + '\nLength: ' + item.runtime + ' mins',
+      body: 'Network: ' + item.network + '\nTime: ' + item.airtime + '\nLength: ' + item.runtime + ' minutes',
       scrollable: true
     });
     card.show();
   });
+  
   menu.show();  
+  splash.hide();
 },
 function(err) {
-  console.log('Error: ' + err);
-});
+  console.log('Error: ' + err.message);
+});  
+};
+
+var getCountry = function() {
+  ajax({
+    url: 'http://nominatim.openstreetmap.org/reverse?format=json&lat=' + lat + '&lon=' + lon,
+    type: 'json'
+  }, function(data) {
+    country_code = data.address.country_code.toUpperCase();
+    country = data.address.country;
+    tvmaze();
+  }, function(err) {
+    country_code = 'US';
+    country = 'United States of America';
+    tvmaze();
+  });
+};
+
+getCountry();
+    
+}
