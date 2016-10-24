3e1de065e9fcad05b2b2d279e33169528e8c9553
J. Tran <j.yn.tran@gmail.com>
2016-10-24 00:44:00 -0400

    Basic schedule functionality

--- src/js/app.js
+++ src/js/app.js
@@ -5,82 +5,45 @@
  */
 
 var UI = require('ui');
-var Vector2 = require('vector2');
+var ajax = require('ajax');
 
-var main = new UI.Card({
-  title: 'Pebble.js',
-  subtitle: 'Hello World!',
-  body: 'Press any button.',
-  subtitleColor: 'indigo', // Named colors
-  bodyColor: '#9a0036' // Hex colors
-});
-
-main.show();
-
-main.on('click', 'up', function(e) {
+ajax({
+  url: 'http://api.tvmaze.com/schedule?country=CA',
+  type: 'json'
+},
+function(data) {
+  var schedule = data.map(function(obj) {
+    return {
+      show: obj.show.name,
+      name: obj.name,
+      airtime: obj.airtime,
+      runtime: obj.runtime
+    };
+  });
+  
   var menu = new UI.Menu({
     sections: [{
-      items: [{
-        title: 'Pebble.js',
-        subtitle: 'Can do Menus'
-      }, {
-        title: 'Second Item',
-        subtitle: 'Subtitle Text'
-      }, {
-        title: 'Third Item',
-      }, {
-        title: 'Fourth Item',
-      }]
+      items: schedule.map(function(obj) {
+        return {
+          title: obj.show,
+          subtitle: obj.airtime
+        };
+      })
     }]
   });
+     
   menu.on('select', function(e) {
-    console.log('Selected item #' + e.itemIndex + ' of section #' + e.sectionIndex);
-    console.log('The item is titled "' + e.item.title + '"');
+    var item  = schedule[e.itemIndex];
+    var card = new UI.Card({
+      title: item.show,
+      subtitle: item.name,
+      body: 'Time: ' + item.airtime + '\nLength: ' + item.runtime + ' mins',
+      scrollable: true
+    });
+    card.show();
   });
-  menu.show();
-});
-
-main.on('click', 'select', function(e) {
-  var wind = new UI.Window({
-    backgroundColor: 'black'
-  });
-  var radial = new UI.Radial({
-    size: new Vector2(140, 140),
-    angle: 0,
-    angle2: 300,
-    radius: 20,
-    backgroundColor: 'cyan',
-    borderColor: 'celeste',
-    borderWidth: 1,
-  });
-  var textfield = new UI.Text({
-    size: new Vector2(140, 60),
-    font: 'gothic-24-bold',
-    text: 'Dynamic\nWindow',
-    textAlign: 'center'
-  });
-  var windSize = wind.size();
-  // Center the radial in the window
-  var radialPos = radial.position()
-      .addSelf(windSize)
-      .subSelf(radial.size())
-      .multiplyScalar(0.5);
-  radial.position(radialPos);
-  // Center the textfield in the window
-  var textfieldPos = textfield.position()
-      .addSelf(windSize)
-      .subSelf(textfield.size())
-      .multiplyScalar(0.5);
-  textfield.position(textfieldPos);
-  wind.add(radial);
-  wind.add(textfield);
-  wind.show();
-});
-
-main.on('click', 'down', function(e) {
-  var card = new UI.Card();
-  card.title('A Card');
-  card.subtitle('Is a Window');
-  card.body('The simplest window type in Pebble.js.');
-  card.show();
+  menu.show();  
+},
+function(err) {
+  console.log('Error: ' + err);
 });
